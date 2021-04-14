74ab864487b5f56cd1a79ba420bb89c221fc9254
Gnoss-song <silmxmail@naver.com>
2021-04-14 17:59:19 +0900

    fragment-my-04/14

--- Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/activity/MainActivity.kt
+++ Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/activity/MainActivity.kt
@@ -1,14 +1,35 @@
 package kr.co.mapo.project_seoulmatcheap.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.view.View
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentTransaction
 import kr.co.mapo.project_seoulmatcheap.R
+import kr.co.mapo.project_seoulmatcheap.databinding.ActivityMainBinding
+import kr.co.mapo.project_seoulmatcheap.ui.fragment.MY_01
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var my01fragment: MY_01
+
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        my01fragment = MY_01()
 
+        binding.button.setOnClickListener {
+
+            val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+            ft.add(R.id.framelayout, my01fragment)
+            ft.commit()
+            binding.button.visibility = View.GONE
+        }
     }
 }
