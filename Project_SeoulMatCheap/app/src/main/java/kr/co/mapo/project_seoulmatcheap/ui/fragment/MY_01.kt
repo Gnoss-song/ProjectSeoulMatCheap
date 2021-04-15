349016b4af7da82a2a94b0a06e663f9fb49fe09f
SANDY <72983926+SANDY-9@users.noreply.github.com>
2021-04-15 13:31:21 +0900

    bottom navigation

--- Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/fragment/MY_01.kt
+++ Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/fragment/MY_01.kt
@@ -9,27 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat.setAlpha
-import com.skydoves.balloon.*
 import kr.co.mapo.project_seoulmatcheap.R
 
 class MY_01 : Fragment() {
 
-    val balloon = createBalloon(requireContext()) {
-        setArrowSize(10)
-        setWidth(BalloonSizeSpec.WRAP)
-        setHeight(65)
-        setArrowPosition(0.7f)
-        setCornerRadius(4f)
-        setAlpha(0.9f)
-        setText("You can access your profile from now on.")
-        setTextColorResource(R.color.white_93)
-        setTextIsHtml(true)
-        setIconDrawable(ContextCompat.getDrawable(context, R.drawable.ic_profile))
-        setBackgroundColorResource(R.color.colorPrimary)
-        setOnBalloonClickListener(onBalloonClickListener)
-        setBalloonAnimation(BalloonAnimation.FADE)
-        setLifecycleOwner(lifecycleOwner)
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
