29d497f07712b9762d7b41bf776c3ad37b394248
SANDY <72983926+SANDY-9@users.noreply.github.com>
2021-04-15 18:15:16 +0900

    0415커밋

--- Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/fragment/MAP_01.kt
+++ Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/fragment/MAP_01.kt
@@ -1,18 +1,37 @@
 package kr.co.mapo.project_seoulmatcheap.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import kr.co.mapo.project_seoulmatcheap.R
+import kr.co.mapo.project_seoulmatcheap.databinding.FragmentMap01Binding
+
 class MAP_01 : Fragment() {
+
+    private lateinit var binding : FragmentMap01Binding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map_01, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_map_01, container,false)
+        binding.fragment = this
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    fun go(v:View) {
+        requireActivity().supportFragmentManager.beginTransaction().replace(R.id.container, MAP_01_02())
+            .addToBackStack(null).commit()
+    }
+
 
 }
