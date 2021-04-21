4dc8aca71b09fe0048451c4f2c5513768a80138f
Gnoss-song <silmxmail@naver.com>
2021-04-21 17:05:59 +0900

    feature_my_0421

--- Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/activity/MY_01_01.kt
+++ Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/activity/MY_01_01.kt
@@ -33,18 +33,25 @@ class MY_01_01 : AppCompatActivity() {
         binding = ActivityMy0101Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val itemData = mutableListOf<Item>()
+        itemData.add(Item(R.drawable.solip, "솔잎식당", "서울특별시 마포구 마포대로4길 46 (도화동)", "한식", "1.1km", "4.8"))
+        itemData.add(Item(R.drawable.poonyeon, "풍년갈비", "서울특별시 마포구 동교로 264 (연남동", "한식", "1.4km", "4.4"))
+        itemData.add(Item(R.drawable.wellbeing, "웰빙뚝배기", "서울특별시 마포구 동교로12길 21 (서교동)", "한식", "2.2km", "3.3"))
+        itemData.add(Item(R.drawable.western, "웨스턴후라이드라이스", "서울특별시 마포구 홍익로 26 (동교동)", "일식", "0.5km", "4.5"))
+        itemData.add(Item(R.drawable.halbum, "할범탕수육", "서울특별시 양천구 목동중앙북로 15 (목동)", "분식", "4.4km", "5.0"))
 
-            val itemData = mutableListOf<Item>()
-            itemData.add(Item(R.drawable.solip, "솔잎식당", "서울특별시 마포구 마포대로4길 46 (도화동)", "한식", "1.1km", "4.8"))
-            itemData.add(Item(R.drawable.poonyeon, "풍년갈비", "서울특별시 마포구 동교로 264 (연남동", "한식", "1.4km", "4.4"))
-            itemData.add(Item(R.drawable.wellbeing, "웰빙뚝배기", "서울특별시 마포구 동교로12길 21 (서교동)", "한식", "2.2km", "3.3"))
-            itemData.add(Item(R.drawable.western, "웨스턴후라이드라이스", "서울특별시 마포구 홍익로 26 (동교동)", "일식", "0.5km", "4.5"))
-            itemData.add(Item(R.drawable.halbum, "할범탕수육", "서울특별시 양천구 목동중앙북로 15 (목동)", "분식", "4.4km", "5.0"))
-
+        //리사이클러뷰 어댑터 연결
         val adapter = InformDetailAdapter(itemData)
         binding.recycler.adapter = adapter
 
         binding.recycler.layoutManager = LinearLayoutManager(this)
+
+        //화면 이동 MY_01_01_01
+
+        binding.btnEdit.setOnClickListener {
+            val intent = Intent(this,MY_01_01_01::class.java)
+            startActivity(intent)
+        }
 
         }
 }
