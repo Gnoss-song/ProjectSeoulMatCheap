ea3efe86b27e32197cea3647410984d0e282e15e
Gnoss-song <silmxmail@naver.com>
2021-04-29 15:48:19 +0900

    feature-my-0429

--- Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/activity/MY_01_02.kt
+++ Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/activity/MY_01_02.kt
@@ -6,6 +6,7 @@ package kr.co.mapo.project_seoulmatcheap.ui.activity
  * @desc
  */
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -29,21 +30,20 @@ class MY_01_02 : AppCompatActivity() {
         setContentView(binding.root)
 
         val list = mutableListOf(
-                Model(Model.IMAGE_TYPE, "성수칼국수", "2021.04.03", 3.5f,R.drawable.halbum,null,null,R.string.review1.toString()),
-                Model(Model.IMAGE_TYPE2, "모르겠어집", "2021.03.15", 4.5f,R.drawable.solip,R.drawable.btn_homeaddress,null,R.string.review2.toString()),
-                Model(Model.IMAGE_TYPE3, "맛있어요집", "2021.03.10", 4.0f,R.drawable.poonyeon,R.drawable.custom_checkbox,R.drawable.solip,"만나서 반갑습니다"),
-                Model(Model.IMAGE_TYPE3, "1번입니다", "2021.03.07", 2.0f,R.drawable.poonyeon,R.drawable.solip,R.drawable.wellbeing,"만나서 반갑습니다"),
-                Model(Model.IMAGE_TYPE, "2번입니다", "2021.02.10", 3.5f,R.drawable.wellbeing,null,null,"만나서 반갑습니다\n어쩌구저쩌구\n아우람ㅇ누람니러아러ㅏㄴ\n다음에또올게요."),
-                Model(Model.IMAGE_TYPE2, "3번입니다", "2021.01.15", 3.0f,R.drawable.halbum,R.drawable.poonyeon,null,"만나서 반갑습니다"),
-                Model(Model.IMAGE_TYPE2, "4번입니다", "2020.12.23", 1.5f,R.drawable.western,R.drawable.western,null,"만나서 반갑습니다"),
-                Model(Model.IMAGE_TYPE, "5번입니다", "2020.11.03", 5.0f,R.drawable.solip,null,null,"만나서 반갑습니다"),
-                Model(Model.IMAGE_TYPE3, "6번입니다", "2020.07.22", 1.0f,R.drawable.poonyeon,R.drawable.solip,R.drawable.poonyeon,"만나서 반갑습니다"),
-                Model(Model.IMAGE_TYPE, "7번입니다", "2020.12.12", 1.5f,R.drawable.wellbeing,null,null,"만나서 반갑습니다"),
-                Model(Model.IMAGE_TYPE, "8번입니다", "2020.03.03", 4.5f,R.drawable.halbum,null,null,"만나서 반갑습니다"),
-                Model(Model.IMAGE_TYPE2, "9번입니다", "2020.01.03", 4.0f,R.drawable.solip,R.drawable.western,null,"만나서 반갑습니다"),
+                Model(Model.IMAGE_TYPE, "성수칼국수", "2021.04.03", 3.5f,R.drawable.halbum,null,null,getText(R.string.review1).toString()),
+                Model(Model.IMAGE_TYPE2, "모르겠어집", "2021.03.15", 4.5f,R.drawable.solip,R.drawable.btn_homeaddress,null,getText(R.string.review2).toString()),
+                Model(Model.IMAGE_TYPE3, "맛있어요집", "2021.03.10", 4.0f,R.drawable.poonyeon,R.drawable.custom_checkbox,R.drawable.solip,getText(R.string.review3).toString()),
+                Model(Model.IMAGE_TYPE3, "1번입니다", "2021.03.07", 2.0f,R.drawable.poonyeon,R.drawable.solip,R.drawable.wellbeing,getText(R.string.review4).toString()),
+                Model(Model.IMAGE_TYPE, "2번입니다", "2021.02.10", 3.5f,R.drawable.wellbeing,null,null,getText(R.string.review5).toString()),
+                Model(Model.IMAGE_TYPE2, "3번입니다", "2021.01.15", 3.0f,R.drawable.halbum,R.drawable.poonyeon,null,getText(R.string.review6).toString()),
+                Model(Model.IMAGE_TYPE2, "4번입니다", "2020.12.23", 1.5f,R.drawable.western,R.drawable.western,null,getText(R.string.review7).toString()),
+                Model(Model.IMAGE_TYPE, "5번입니다", "2020.11.03", 5.0f,R.drawable.solip,null,null,getText(R.string.review8).toString()),
+                Model(Model.IMAGE_TYPE3, "6번입니다", "2020.07.22", 1.0f,R.drawable.poonyeon,R.drawable.solip,R.drawable.poonyeon,getText(R.string.review9).toString()),
+                Model(Model.IMAGE_TYPE, "7번입니다", "2020.12.12", 1.5f,R.drawable.wellbeing,null,null,getText(R.string.review10).toString()),
+                Model(Model.IMAGE_TYPE, "8번입니다", "2020.03.03", 4.5f,R.drawable.halbum,null,null,getText(R.string.review11).toString()),
+                Model(Model.IMAGE_TYPE2, "9번입니다", "2020.01.03", 4.0f,R.drawable.solip,R.drawable.western,null,getText(R.string.review12).toString()),
 
         )
-
         val  dividerItemDecoration =
                 DividerItemDecoration(binding.recycler3.context, LinearLayoutManager(this).orientation)
 
