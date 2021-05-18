cf3d92350474289790204159f4bd0c1689b9881a
KHJ-11 <72050086+KHJ-11@users.noreply.github.com>
2021-05-18 12:02:36 +0900

    category0518

--- Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/activity/INFORM_02.kt
+++ Project_SeoulMatCheap/app/src/main/java/kr/co/mapo/project_seoulmatcheap/ui/activity/INFORM_02.kt
@@ -1,7 +1,9 @@
 package kr.co.mapo.project_seoulmatcheap.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kr.co.mapo.project_seoulmatcheap.R
@@ -14,6 +16,11 @@ class INFORM_02 : AppCompatActivity() {
         with(findViewById<RecyclerView>(R.id.reviewRecyclerView)) {
             layoutManager = LinearLayoutManager(this@INFORM_02, LinearLayoutManager.HORIZONTAL, false)
             adapter = InfromReviewAdapter()
+        }
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            val intent = Intent(this,INFORM_02_01::class.java)
+            startActivity(intent)
         }
     }
 }
